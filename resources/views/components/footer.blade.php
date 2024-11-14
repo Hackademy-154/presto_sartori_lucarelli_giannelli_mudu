b764e46eca94c92f910eed4726f6a32d4b751eb6
Kureo98 <colmatteox@gmail.com>
2024-11-14 15:53:50 +0100

    inserito footer, colors e fonts

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,148 @@
+<footer class="bg_footer2 text-center">
+    <div class="container p-4">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+            <!-- Facebook -->
+            <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i
+                    class="fab fa-facebook-f"></i></a>
+
+            <!-- Twitter -->
+            <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i
+                    class="fab fa-twitter"></i></a>
+
+            <!-- Google -->
+            <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i
+                    class="fab fa-google"></i></a>
+
+            <!-- Instagram -->
+            <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i
+                    class="fab fa-instagram"></i></a>
+
+            <!-- Linkedin -->
+            <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i
+                    class="fab fa-linkedin-in"></i></a>
+
+            <!-- Github -->
+            <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i
+                    class="fab fa-github"></i></a>
+        </section>
+
+        <!-- Section: Form -->
+        <section class="">
+            <form action="">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-auto">
+                        <p class="pt-2">
+                            <strong>Sign up for our newsletter</strong>
+                        </p>
+                    </div>
+                    <div class="col-md-5 col-12">
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <input type="email" id="form5Example24" class="form-control" />
+                            <label class="form-label" for="form5Example24">Email address</label>
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <button data-mdb-ripple-init type="submit" class="btn btn-outline mb-4">
+                            Subscribe
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </section>
+
+        <!-- Section: Text -->
+        <section class="mb-4">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+                repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam eum
+                harum corrupti dicta, aliquam sequi voluptate quas.
+            </p>
+        </section>
+
+        <!-- Section: Links -->
+        <section class="">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a class="text-body" href="#!">Link 1</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 2</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 3</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a class="text-body" href="#!">Link 1</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 2</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 3</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a class="text-body" href="#!">Link 1</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 2</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 3</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a class="text-body" href="#!">Link 1</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 2</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 3</a>
+                        </li>
+                        <li>
+                            <a class="text-body" href="#!">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </section>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center p-3 bg_footer1">
+        © 2024 Copyright:
+        <a class="text-reset fw-bold" href="http://127.0.0.1:8000/">Presto.it</a>
+    </div>
+</footer>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+        rel="stylesheet">
+    {{-- Vite --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>Document</title>
+    <title>Presto</title>
 </head>
+
 <body>
     <x-navbar></x-navbar>
     <div class="min-vh-100">
-        {{$slot}}
+        {{ $slot }}
     </div>
     <x-footer></x-footer>
 </body>
+
 </html>
