813a99f7f76ad6f0090abf16a8734005a57db3b1
Kureo98 <colmatteox@gmail.com>
2024-11-15 11:00:50 +0100

    aggiunte prototipo card e test navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,13 @@
 <x-layout>
-    <div class="container-fluid text-center bg-body-tertiary">
+    <div class="container text-center bg-body-tertiary">
         <div class="row">
             <!-- CAROUSEL -->
-            <div class="col-12 bg-danger mt-3">
+            <div class="col-12 p-0">
                 <div id="carouselExampleFade" class="carousel slide carousel-fade">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="..." class="d-block w-100" alt="...">IMMAGINE1
+                            <img src="{{ Storage::url('image/slideAbbigliamento.jpg') }}" class="d-block w-100"
+                                alt="...">
                         </div>
                         <div class="carousel-item">
                             <img src="..." class="d-block w-100" alt="...">IMMAGINE2
@@ -15,11 +16,13 @@
                             <img src="..." class="d-block w-100" alt="...">IMMAGINE3
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade"
+                        data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade"
+                        data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
@@ -27,41 +30,153 @@
             </div>
         </div>
         <!-- FINE CAROUSEL -->
+        <div class="row  justify-content-center align-items-center mt-5">
 
-
-        <div class="row vh-100 justify-content-evenly align-items-start mt-5">
-
-            <div class="col-2 mx-2">
-                <div class="accordion" id="accordionPanelsStayOpenExample">
-                    <div class="accordion-item ">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                                <a href="{{route('article.create')}}" class="text-decoration-none text-black">Aggiungi articolo</a>
-                            </button>
-                        </h2>
-                        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
-                            <div class="accordion-body">
-                                <livewire:create-article-form>
-                            </div>
-                        </div>
+            <div class="col-4 m-2 cardCustom">
+                <div class="row justify-content-between h-100 pb-3">
+                    <div class="col-12 px-2 pt-2"><img src="{{ Storage::url('image/slideAbbigliamento.jpg') }}"
+                            alt="" class="img-fluid imgCard"></div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold mt-2">Felpa Nike limited edition</h5>
+                    </div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold">$349.95</h5>
+                    </div>
+                    <div class="col-12  d-flex text-start">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum fugiat quidem veniam
+                        perferendis iure tenetur, nobis ullam...
+                    </div>
+                    <div class="col-12  d-flex justify-content-end align-items-end mt-2 ">
+                        <button class="buttonCard">Vai al dettaglio</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 m-2 cardCustom">
+                <div class="row justify-content-between h-100 pb-3">
+                    <div class="col-12 px-2 pt-2"><img src="{{ Storage::url('image/slideAbbigliamento.jpg') }}"
+                            alt="" class="img-fluid imgCard"></div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold mt-2">Felpa Nike limited edition</h5>
+                    </div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold">$349.95</h5>
+                    </div>
+                    <div class="col-12  d-flex text-start">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum fugiat quidem veniam
+                        perferendis iure tenetur, nobis ullam...
+                    </div>
+                    <div class="col-12  d-flex justify-content-end align-items-end mt-2 ">
+                        <button class="buttonCard">Vai al dettaglio</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 m-2 cardCustom">
+                <div class="row justify-content-between h-100 pb-3">
+                    <div class="col-12 px-2 pt-2"><img src="{{ Storage::url('image/slideAbbigliamento.jpg') }}"
+                            alt="" class="img-fluid imgCard"></div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold mt-2">Felpa Nike limited edition</h5>
+                    </div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold">$349.95</h5>
+                    </div>
+                    <div class="col-12  d-flex text-start">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum fugiat quidem veniam
+                        perferendis iure tenetur, nobis ullam...
+                    </div>
+                    <div class="col-12  d-flex justify-content-end align-items-end mt-2 ">
+                        <button class="buttonCard">Vai al dettaglio</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 m-2 cardCustom">
+                <div class="row justify-content-between h-100 pb-3">
+                    <div class="col-12 px-2 pt-2"><img src="{{ Storage::url('image/slideAbbigliamento.jpg') }}"
+                            alt="" class="img-fluid imgCard"></div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold mt-2">Felpa Nike limited edition</h5>
+                    </div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold">$349.95</h5>
+                    </div>
+                    <div class="col-12  d-flex text-start">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum fugiat quidem veniam
+                        perferendis iure tenetur, nobis ullam...
+                    </div>
+                    <div class="col-12  d-flex justify-content-end align-items-end mt-2 ">
+                        <button class="buttonCard">Vai al dettaglio</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 m-2 cardCustom">
+                <div class="row justify-content-between h-100 pb-3">
+                    <div class="col-12 px-2 pt-2"><img src="{{ Storage::url('image/slideAbbigliamento.jpg') }}"
+                            alt="" class="img-fluid imgCard"></div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold mt-2">Felpa Nike limited edition</h5>
+                    </div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold">$349.95</h5>
+                    </div>
+                    <div class="col-12  d-flex text-start">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum fugiat quidem veniam
+                        perferendis iure tenetur, nobis ullam...
+                    </div>
+                    <div class="col-12  d-flex justify-content-end align-items-end mt-2 ">
+                        <button class="buttonCard">Vai al dettaglio</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 m-2 cardCustom">
+                <div class="row justify-content-between h-100 pb-3">
+                    <div class="col-12 px-2 pt-2"><img src="{{ Storage::url('image/slideAbbigliamento.jpg') }}"
+                            alt="" class="img-fluid imgCard"></div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold mt-2">Felpa Nike limited edition</h5>
+                    </div>
+                    <div class="col-12  text-start">
+                        <h5 class="fw-bold">$349.95</h5>
+                    </div>
+                    <div class="col-12  d-flex text-start">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum fugiat quidem veniam
+                        perferendis iure tenetur, nobis ullam...
+                    </div>
+                    <div class="col-12  d-flex justify-content-end align-items-end mt-2 ">
+                        <button class="buttonCard">Vai al dettaglio</button>
                     </div>
                 </div>
             </div>
 
-            <div class="col-9">
 
-                <div class="cardExepmle rounded-3 d-flex flex-column justify-content-evenly align-items-center">
-                    <div class="immagine mt-2"></div>
-                    <div class="d-flex flex-row justify-content-between mt-1 px-3 w-100">
-                        <h5>TITOLO</h5>
-                        <p>20€</p>
-                    </div>
-                    <p>Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem, ipsum</p>
-                    <a href="" class="btn btn-dark rounded-4 w-75 mb-2">Vai</a>
+        </div>
+    </div>
+</x-layout>
+{{-- Da rivedere --}}
+{{-- <div class=" col-4 m-2 cardExepmle rounded-3 d-flex flex-column justify-content-evenly align-items-center">
+    <div class="immagine mt-2"></div>
+    <div class="d-flex flex-row justify-content-between mt-1 ">
+        <h5>TITOLO</h5>
+        <p>20€</p>
+    </div>
+    <p>Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem, ipsum</p>
+    <a href="" class="btn btn-dark rounded-4 w-75 mb-2">Vai</a>
+</div> --}}
+{{-- <div class="col-2 mx-2">
+    <div class="accordion" id="accordionPanelsStayOpenExample">
+        <div class="accordion-item ">
+            <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
+                    aria-controls="panelsStayOpen-collapseOne">
+                    <a href="{{ route('article.create') }}" class="text-decoration-none text-black">Aggiungi
+                        articolo</a>
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                <div class="accordion-body">
+                    <livewire:create-article-form>
                 </div>
-
             </div>
         </div>
     </div>
-
-</x-layout>
+</div> --}}
