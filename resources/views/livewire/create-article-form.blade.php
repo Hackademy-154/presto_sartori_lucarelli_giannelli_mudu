5d5410934f58bc5df08c609b52d0d78d4f93c816
Federica Mudu <federica.mudu.1993@gmail.com>
2024-11-15 10:04:09 +0100

    aggiunto feedback visivo per annuncio inserito

--- resources/views/livewire/create-article-form.blade.php
+++ resources/views/livewire/create-article-form.blade.php
@@ -1,34 +1,39 @@
 <form class="shadow p-5 bg-white text-center" wire:submit="save">
+    @if (session('success'))
+    <div class="alert alert-success text-center">
+        {{ session('success') }}
+    </div>
+    @endif
     <div class="mb-5">
         <label for="title" class="form-label text-dark">Titolo</label>
         <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" wire:model.blur="title">
         @error('title')
-            <p class="fst-italic text-danger">{{$message}}</p>
+        <p class="fst-italic text-danger">{{$message}}</p>
         @enderror
     </div>
     <div class="mb-5">
         <label for="description" class="form-label text-dark">Descrizione</label>
         <textarea class="form-control @error('description') is-invalid @enderror" id="description" wire:model.blur="description"></textarea>
         @error('description')
-            <p class="fst-italic text-danger">{{$message}}</p>
+        <p class="fst-italic text-danger">{{$message}}</p>
         @enderror
     </div>
     <div class="mb-5">
         <label for="price" class="form-label text-dark">Prezzo</label>
         <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" wire:model.blur="price">
         @error('price')
-            <p class="fst-italic text-danger">{{$message}}</p>
+        <p class="fst-italic text-danger">{{$message}}</p>
         @enderror
     </div>
     <div class="mb-5">
         <select id="category" wire:model.blur="category" class="form-control">
             <option label disabled> Seleziona una categoria</option>
             @foreach ($categories as $category)
-                <option value="{{$category->id}}" >{{$category->name}}</option>
+            <option value="{{$category->id}}">{{$category->name}}</option>
             @endforeach
         </select>
         @error('category')
-            <p class="fst-italic text-danger">{{$message}}</p>
+        <p class="fst-italic text-danger">{{$message}}</p>
         @enderror
 
     </div>
