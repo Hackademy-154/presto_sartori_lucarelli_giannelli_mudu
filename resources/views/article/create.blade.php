8bbdb7cfac170485264cb060336f56a051a023ed
Matteo Lucarelli <matteo.lucarelli93@gmail.com>
2024-11-15 13:26:13 +0100

    modificato font e pagina di create

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>
+                <h1 class="text-center text-dark fw-bold m-5">
                     Inserire articolo
                 </h1>
             </div>
--- resources/views/livewire/create-article-form.blade.php
+++ resources/views/livewire/create-article-form.blade.php
@@ -1,31 +1,33 @@
-<form class="shadow p-5 bg-white text-center" wire:submit="save">
+<form class="shadow p-5 pb-2 bg-tertiary rounded-3 text-center" wire:submit="save">
     @if (session('success'))
     <div class="alert alert-success text-center">
         {{ session('success') }}
     </div>
     @endif
-    <div class="mb-5">
+
+    <div class="mb-3 text-start">
         <label for="title" class="form-label text-dark">Titolo</label>
         <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" wire:model.blur="title">
         @error('title')
         <p class="fst-italic text-danger">{{$message}}</p>
         @enderror
     </div>
-    <div class="mb-5">
+    <div class="mb-3 text-start">
         <label for="description" class="form-label text-dark">Descrizione</label>
         <textarea class="form-control @error('description') is-invalid @enderror" id="description" wire:model.blur="description"></textarea>
         @error('description')
         <p class="fst-italic text-danger">{{$message}}</p>
         @enderror
     </div>
-    <div class="mb-5">
+    <div class="mb-3 text-start">
         <label for="price" class="form-label text-dark">Prezzo</label>
         <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" wire:model.blur="price">
         @error('price')
         <p class="fst-italic text-danger">{{$message}}</p>
         @enderror
     </div>
-    <div class="mb-5">
+    <div class="mb-3 text-start">
+        <label for="category" class="form-label text-dark">Categoria</label>
         <select id="category" wire:model.blur="category" class="form-control">
             <option label> Seleziona una categoria</option>
             @foreach ($categories as $category)
@@ -37,7 +39,7 @@
         @enderror
 
     </div>
-    <div class="mb-5 text-center">
-        <button type="submit" class="btn btn-dark">Crea</button>
+    <div class="mb-3 text-center">
+        <button type="submit" class="btn btn-dark w-25 mt-3">Crea</button>
     </div>
 </form>
