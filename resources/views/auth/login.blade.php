6f5ed22aa12298726677823477fc02c32ddf7d72
Federica Mudu <federica.mudu.1993@gmail.com>
2024-11-19 16:12:08 +0100

    aggiunti messaggi errore campi vuotu obbligatori in login e register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,11 +21,17 @@
                     @csrf
                     <div class="d-flex flex-column">
                         <label class="fw-bold" for="email ">Email:</label>
-                        <input type="email" name="email" class="my-1  p-1" />
+                        <input type="email" name="email" class="my-1  p-1 @error('email') is-invalid @enderror">
+                        @error('email')
+                        <div class="text-danger fst-italic">Indirizzo email non valido</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column ">
                         <label class="fw-bold" for="password ">Password:</label>
-                        <input type="password" name="password" class="my-1 p-1" />
+                        <input type="password" name="password" class="my-1 p-1 @error('password') is-invalid @enderror">
+                        @error('password')
+                        <div class="text-danger fst-italic">Password non valida</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column justify-content-center text-center mt-1">
                         <span class="fst-italic ">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,19 +21,31 @@
                     @csrf
                     <div class="d-flex flex-column">
                         <label class="fw-bold" for="name">Name:</label>
-                        <input type="text" name="name" class="mb-2 p-1" />
+                        <input type="text" name="name" class="mb-2 p-1 @error('name') is-invalid @enderror">
+                        @error('name')
+                        <div class="text-danger fst-italic">Nome obbligatorio</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column">
                         <label class="fw-bold" for="email">Email<span class="text-danger">*</span></label>
-                        <input type="email" name="email" class="mb-2 p-1" />
+                        <input type="email" name="email" class="mb-2 p-1 @error('email') is-invalid @enderror">
+                        @error('email')
+                        <div class="text-danger fst-italic">Indirizzo email obbligatorio</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column">
                         <label class="fw-bold" for="password">Password:</label>
-                        <input type="password" name="password" class="mb-2 p-1" />
+                        <input type="password" name="password" class="mb-2 p-1 @error('password') is-invalid @enderror">
+                        @error('password')
+                        <div class="text-danger fst-italic">Password non valida</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column">
                         <label class="fw-bold"for="password_confirmation">Confirm Password:</label>
-                        <input type="password" name="password_confirmation" class="mb-2 p-1" />
+                        <input type="password" name="password_confirmation" class="mb-2 p-1 @error('password') is-invalid @enderror">
+                        @error('password')
+                        <div class="text-danger fst-italic">Password non valida</div>
+                        @enderror
                     </div>
                     <div class="d-flex flex-column justify-content-center text-center mt-1">
 
