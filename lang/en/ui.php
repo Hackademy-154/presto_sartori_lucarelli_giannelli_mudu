f009f6f8ebd7bfcfeb20aaaad3e69189065381e5
Matteo Lucarelli <matteo.lucarelli93@gmail.com>
2024-11-21 18:19:43 +0100

    aggiunto robe della lingua

--- lang/en/ui.php
+++ lang/en/ui.php
@@ -1,5 +1,58 @@
 <?php
 return [
-    'hello' => 'Hello, sign in',
-    'allArticles' => 'All the articles'
+    'hello' => 'Hello,',
+    'allArticles' => 'All articles',
+    'category' => 'Category',
+    'categories' => 'Categories',
+        'abbigliamento' => 'Clothes',
+        'accessori' => 'Accessories',
+        'animali domestici' => 'Pets',
+        'casa e giardinaggio' => 'Home & Garden',
+        'elettronica' => 'Electronics',
+        'giocattoli' => 'Toys',
+        'libri e riviste' => 'Books & Magazines',
+        'motori' => 'Engines',
+        'salute e bellezza' => 'Health & Beauty',
+        'sport' => 'Sport',
+    'welcome' => 'Welcome',
+    'slogan1' => 'Your shopping, our effort',
+    'text1' => 'Discover a world of carefully selected products to satisfy your every need.
+                We are here to offer you a simple, safe and pleasant shopping experience.
+                Explore our offers, enjoy exclusive benefits and let your next adventure begin with us. Quality and your satisfaction are our priority!',
+    'btnDiscoverAds' => 'Discover ads',
+    'addAds' => 'Add Items',
+    'slogan2' => 'Without wasting time',
+    'text2' => 'Publish your ads in a few simple steps. Our platform is designed to save you valuable time, allowing you to focus on what really matters.
+                Don \'t wait, start now!',
+    'btnAddAds' => 'Add ads',
+    'fastDelivery' => 'Fast Delivery',
+        'textDelivery' => 'Fast and efficient delivery service.',
+    'securePayment' => 'Secure Payment',
+        'textPayment' => 'Reliable and protected payment methods.',
+    'quality' => 'Quality Guaranteed',
+        'textQuality' => 'We guarantee the quality of our products.',
+    'customerService' => 'Customer Service',
+        'textService' => 'Assistance service always available.',
+    'btnShow' => 'Show article',
+    'cart'  => 'Cart',
+    'btnRevisor' => 'Revisor area',
+    'logout' => 'Logout',
+    'search' => 'Search Article',
+    'contactUs' => 'Contact us',
+    'faq' => 'FAQ',
+    
+    'title' => 'Title',
+    'placeholder1' => 'Insert title',
+    'description' => 'Description',
+    'placeholder2' => 'Insert description',
+    'price' => 'Price',
+    'placeholder3' => '12.5€',
+    'selectCategory' => 'Select category',
+    'btnBack' => 'Back',
+    'btnAdd' => 'Add',
+
+    'titleIndex' => 'Articles of the category',
+    'login' => 'Login',     
+    
+
 ];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,23 +9,17 @@
                         <div class="row h-100">
                             <div class="col-6 d-flex flex-column justify-content-center p-5 headerSX">
                                 <div class="title" data-swiper-parallax="-300">
-                                    <h1 class="fw-bold">Benvenuto</h1>
+                                    <h1 class="fw-bold">{{__('ui.welcome')}}</h1>
                                 </div>
                                 <div class="subtitle" data-swiper-parallax="-200">
-                                    <h3 class="pb-3">Il tuo shopping, il nostro impegno</h3>
+                                    <h3 class="pb-3">{{__('ui.slogan1')}}</h3>
                                 </div>
                                 <div class="text" data-swiper-parallax="-100">
                                     <p>
-                                        Scopri un mondo di prodotti selezionati con cura per soddisfare ogni tua
-                                        esigenza.
-                                        Siamo qui per offrirti un'esperienza di acquisto semplice, sicura e piacevole.
-                                        Esplora le nostre offerte, approfitta di vantaggi esclusivi e lascia che la tua
-                                        prossima avventura inizi con noi. La qualità e la tua soddisfazione sono la
-                                        nostra priorità!
+                                    {{__('ui.text1')}}
                                     </p>
                                 </div>
-                                <a href="{{ route('article.index') }}" class="btn buttonCharacter fs-5 mt-2">Scopri
-                                    Annunci</a>
+                                <a href="{{ route('article.index') }}" class="btn buttonCharacter fs-5 mt-2">{{__('ui.btnDiscoverAds')}}</a>
                             </div>
                             <div class="col-6 d-flex justify-content-start align-items-center">
                                 <img class="img-fluid imgHeader" src="{{ Storage::url('image/header.png') }}"
@@ -37,24 +31,17 @@
                         <div class="row h-100">
                             <div class="col-6 d-flex flex-column justify-content-center p-5 headerSX">
                                 <div class="title" data-swiper-parallax="-300">
-                                    <h1>Inserisci Annuncio</h1>
+                                    <h1>{{__('ui.addAds')}}</h1>
                                 </div>
                                 <div class="subtitle" data-swiper-parallax="-200">
-                                    <h2>Senza perdere tempo</h2>
+                                    <h2>{{__('ui.slogan2')}}</h2>
                                 </div>
                                 <div class="text" data-swiper-parallax="-100">
                                     <p>
-                                        Pubblica i tuoi annunci in pochi semplici passaggi. La nostra piattaforma è
-                                        pensata
-                                        per
-                                        farti risparmiare tempo prezioso, permettendoti di concentrarti su ciò che conta
-                                        davvero.
-                                        Non aspettare, inizia ora!
+                                    {{__('ui.text2')}}
                                     </p>
                                 </div>
-                                <a href="{{ route('article.create') }}" class="btn buttonCharacter mt-2 fs-5">Aggiungi
-                                    un
-                                    articolo</a>
+                                <a href="{{ route('article.create') }}" class="btn buttonCharacter mt-2 fs-5">{{__('ui.btnAddAds')}}</a>
                             </div>
                             <div class="col-6 d-flex justify-content-start align-items-center">
                                 <img class="img-fluid imgHeader2 " src="{{ Storage::url('image/header2.png') }}"
@@ -76,39 +63,36 @@
             <div class="col-6 col-md-3 pt-2">
                 <div class="infoIcon">
                     <i class="bi bi-truck fs-1 barraIcon  mb-3"></i>
-                    <h5>Spedizione Veloce</h5>
+                    <h5>{{__('ui.fastDelivery')}}</h5>
                     <div class="divisorioIcons"></div>
-                    <p>Servizio di consegna rapido
-                        ed efficiente.</p>
+                    <p>{{__('ui.textDelivery')}}.</p>
                 </div>
             </div>
             {{-- Punto di forza 2 --}}
             <div class="col-6 col-md-3 pt-2">
                 <div class="infoIcon">
                     <i class="bi bi-lock fs-1 barraIcon  mb-3"></i>
-                    <h5>Pagamento Sicuro</h5>
+                    <h5>{{__('ui.securePayement')}}</h5>
                     <div class="divisorioIcons"></div>
-                    <p>Metodi di pagamento affidabili e
-                        protetti.</p>
+                    <p>{{__('ui.textPayement')}}</p>
                 </div>
             </div>
             {{-- Punto di forza 3 --}}
             <div class="col-6 col-md-3 pt-2">
                 <div class="infoIcon">
                     <i class="bi bi-award fs-1 barraIcon  mb-3"></i>
-                    <h5>Qualità Garantita</h5>
+                    <h5>{{__('ui.quality')}}</h5>
                     <div class="divisorioIcons"></div>
-                    <p>Massimo della qualità e della
-                        soddisfazione.</p>
+                    <p>{{__('ui.textQuality')}}</p>
                 </div>
             </div>
             {{-- Punto di forza 4 --}}
             <div class="col-6 col-md-3 pt-2">
                 <div class="infoIcon">
                     <i class="bi bi-headset fs-1 barraIcon  mb-3"></i>
-                    <h5>Assistenza 24/7</h5>
+                    <h5>{{__('ui.customerService')}}</h5>
                     <div class="divisorioIcons"></div>
-                    <p>Servizio di assistenza sempre disponibile.</p>
+                    <p>{{__('ui.textService')}}</p>
                 </div>
             </div>
         </div>
@@ -118,7 +102,7 @@
     <div class="container text-center cambiacolore sfumaturaa mt-3 mb-4 border">
         <div class="row justify-content-center ">
             <div class="col-12 d-flex justify-content-start align-items-center mb-1 mt-2 sectionsArrow ps-4 pt-3">
-                <h2 class="m-0 textGrey fw-bold ">Categorie</h2><a class="colorPetrolio mt-1"
+                <h2 class="m-0 textGrey fw-bold ">{{__('ui.categories')}}</h2><a class="colorPetrolio mt-1"
                     href="{{ route('article.index') }}"><i
                         class="bi colorPetrolio fw-bold bi-arrow-right-short fs-1 mt-1"></i></a>
             </div>
@@ -129,7 +113,7 @@
                             <a href="{{ route('article.byCategory', compact('category')) }}">
                                 <img src={{ '/media/category_pic/' . $category->id . '.jpg' }}
                                     class="imgCategoryCerchio sfumatura">
-                                <h5 class="text-dark mt-3 fw-bold">{{ ucWords($category->name) }}</h5>
+                                <h5 class="text-dark mt-3 fw-bold">{{ ucFirst(__("ui.$category->name")) }}</h5>
                             </a>
                         </div>
                     @endforeach
