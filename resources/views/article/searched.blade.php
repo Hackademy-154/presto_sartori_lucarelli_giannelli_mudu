67a0e4997cbc5c416ecd0bbb8c9856c549753ddd
Kureo98 <colmatteox@gmail.com>
2024-11-25 12:45:46 +0100

    fix footer

--- resources/views/article/searched.blade.php
+++ resources/views/article/searched.blade.php
@@ -1,20 +1,19 @@
 <x-layout>
-    <div class="container">
-        <div class="row py-5 justify-content-center align-items-center text-center">
+    <div class="container margin-footer pb-5 ">
+        <div class="row py-5 justify-content-center align-items-center text-center mb-5">
             <div class="col-12">
                 <h1 class="display1">
-                    {{ __('ui.resultsFor')}} "<span class="fst-italic">
-                        @if ($query)  
-                            {{ ucfirst($query) }}  
-                        @elseif (isset($category) && $category) 
-                            {{ ucfirst(__('ui.' . $category->name)) }}  
-                        @else  
-                            
+                    {{ __('ui.resultsFor') }} "<span class="fst-italic">
+                        @if ($query)
+                            {{ ucfirst($query) }}
+                        @elseif (isset($category) && $category)
+                            {{ ucfirst(__('ui.' . $category->name)) }}
+                        @else
                         @endif
                     </span>"
                 </h1>
             </div>
-            
+
         </div>
         <div class="row height-custom justify-content-center align-items-center py-5">
             @forelse($articles as $article)
@@ -23,7 +22,7 @@
             @empty
                 <div class="col-12">
                     <h3 class="text-center">
-                        {{__('ui.noResults')}}
+                        {{ __('ui.noResults') }}
                     </h3>
                 </div>
             @endforelse
