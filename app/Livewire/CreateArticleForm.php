f9139e8f39c49a74a8c74bbf80e9682298c8b51d
Matteo Lucarelli <matteo.lucarelli93@gmail.com>
2024-11-26 18:20:21 +0100

    fatto logica google face cazz

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Article;
 use Livewire\Component;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
@@ -51,9 +52,14 @@ class CreateArticleForm extends Component
                 $newImage = $this->article->images()->create([
                     'path' => $image->store($newFileName, 'public')
                 ]);
-                dispatch(new ResizeImage($newImage->path, 1000, 1000 ));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                // dispatch(new ResizeImage($newImage->path, 1000, 1000 ));
+                // dispatch(new GoogleVisionSafeSearch($newImage->id));
+                // dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 1000, 1000 ),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+                ])->dispatch($newImage->id);
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
